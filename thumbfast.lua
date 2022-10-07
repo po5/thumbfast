806903733fba8fee0a972e19dc84853976ed159a
hooke007 <41094733+hooke007@users.noreply.github.com>
2022-10-07 21:09:06 +0100

    fix: improve the decoding

--- thumbfast.lua
+++ thumbfast.lua
@@ -257,7 +257,7 @@ local function spawn(time)
             "--input-ipc-server="..options.socket,
             "--start="..time, "--hr-seek=no",
             "--ytdl-format=worst", "--demuxer-readahead-secs=0", "--demuxer-max-bytes=128KiB",
-            "--vd-lavc-skiploopfilter=all", "--vd-lavc-software-fallback=1", "--vd-lavc-fast",
+            "--vd-lavc-skiploopfilter=all", "--vd-lavc-software-fallback=1", "--vd-lavc-fast", "--vd-lavc-threads=2",
             "--vf="..vf_string(filters_all, true),
             "--sws-allow-zimg=no", "--sws-fast=yes", "--sws-scaler=fast-bilinear",
             "--video-rotate="..last_rotate,
