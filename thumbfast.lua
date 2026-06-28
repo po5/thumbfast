3ced4f1ed65d7bbf86025ff722a09bf333d0d850
Torvin <Torvin@users.noreply.github.com>
2026-06-29 00:02:11 +1200

    make compatible with mpv.net v7.1.2+ (fixes #163)

--- thumbfast.lua
+++ thumbfast.lua
@@ -67,10 +67,15 @@ function subprocess(args, async, callback)
     callback = callback or function() end
 
     if not pre_0_30_0 then
+        local env = {
+            "TMP="..os.getenv("TMP"),
+            "TEMP="..os.getenv("TEMP"),
+            "PATH="..os.getenv("PATH"),
+        }
         if async then
-            return mp.command_native_async({name = "subprocess", playback_only = true, args = args, env = "PATH="..os.getenv("PATH")}, callback)
+            return mp.command_native_async({name = "subprocess", playback_only = true, args = args, env = env}, callback)
         else
-            return mp.command_native({name = "subprocess", playback_only = false, capture_stdout = true, args = args, env = "PATH="..os.getenv("PATH")})
+            return mp.command_native({name = "subprocess", playback_only = false, capture_stdout = true, args = args, env = env})
         end
     else
         if async then
